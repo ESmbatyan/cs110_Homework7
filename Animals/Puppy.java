bcbe23a8b8c0f5975925777c2d85718bba9ab867
ESmbatyan <31730807+ESmbatyan@users.noreply.github.com>
2017-12-04 16:43:59 +0400

    Add files via upload

---
+++ Animals/Puppy.java
@@ -0,0 +1,16 @@
+package Animals;
+
+public class Puppy{
+  
+  private String name;
+  public Puppy (String name){
+    
+    this.name = name;
+    
+  }
+  public String namerReturn(){
+    return this.name;
+  }
+  
+  
+}
